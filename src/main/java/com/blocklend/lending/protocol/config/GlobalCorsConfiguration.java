87c2cd3a74980465c6e4e6de502fd0b05833821b
ebukizy1 <ebukizy1@gmail.com>
2024-05-08 19:20:37 +0100

    update

--- src/main/java/com/blocklend/lending/protocol/config/GlobalCorsConfiguration.java
+++ src/main/java/com/blocklend/lending/protocol/config/GlobalCorsConfiguration.java
@@ -8,17 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalCorsConfiguration {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://email-service-backend-2.onrender.com", "*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                        .allowedOrigins("*")
                         .allowCredentials(true);
             }
         };
     }
 }
+
+
